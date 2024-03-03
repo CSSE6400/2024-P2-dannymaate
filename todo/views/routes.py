c85f7c8c27219baeb08d158a66645f07c35daaa0
dannymaate <dannymaate@hotmail.com>
2024-03-03 16:39:15 +1000

    Follow prac02 handout. Pass all 5 tests. Modify test_todo only to change TODO items to year 2024.

--- todo/views/routes.py
+++ todo/views/routes.py
@@ -1,6 +1,9 @@
-from flask import Blueprint, jsonify
- 
-api = Blueprint('api', __name__, url_prefix='/api/v1') 
+from flask import Blueprint, jsonify, request
+from todo.models import db
+from todo.models.todo import Todo
+from datetime import datetime, timedelta
+
+api = Blueprint('api', __name__, url_prefix='/api/v1')
 
 TEST_ITEM = {
     "id": 1,
@@ -11,8 +14,9 @@ TEST_ITEM = {
     "created_at": "2023-02-20T00:00:00",
     "updated_at": "2023-02-20T00:00:00"
 }
- 
-@api.route('/health') 
+
+
+@api.route('/health')
 def health():
     """Return a status of 'ok' if the server is running and listening to request"""
     return jsonify({"status": "ok"})
@@ -20,26 +24,85 @@ def health():
 
 @api.route('/todos', methods=['GET'])
 def get_todos():
-    """Return the list of todo items"""
-    return jsonify([TEST_ITEM])
+    todos = Todo.query.all()
+    result = []
+    completed_query = request.args.get('completed')
+    window = request.args.get('window')
+
+    for todo in todos:
+        if window:
+            if todo.deadline_at < datetime.now():
+                earlier_date = todo.deadline_at + timedelta(days=float(window))
+                if datetime.now() < earlier_date:
+                    result.append(todo.to_dict())
+
+            elif datetime.now() < todo.deadline_at:
+                later_date = todo.deadline_at - timedelta(days=float(window))
+                if datetime.now() > later_date:
+                    result.append(todo.to_dict())
+            continue
+
+        if completed_query:
+            if str(todo.completed) == completed_query.capitalize():
+                result.append(todo.to_dict())
+        else:
+            result.append(todo.to_dict())
+    return jsonify(result)
+
 
 @api.route('/todos/<int:todo_id>', methods=['GET'])
 def get_todo(todo_id):
-    """Return the details of a todo item"""
-    return jsonify(TEST_ITEM)
+    todo = Todo.query.get(todo_id)
+    if todo is None:
+        return jsonify({'error': 'Todo not found'}), 404
+    return jsonify(todo.to_dict())
+
 
 @api.route('/todos', methods=['POST'])
 def create_todo():
-    """Create a new todo item and return the created item"""
-    return jsonify(TEST_ITEM), 201
+    todo = Todo(
+        title=request.json.get('title'),
+        description=request.json.get('description'),
+        completed=request.json.get('completed', False),
+    )
+    extra = request.json.get('extra')
+    if todo.title is None or extra:
+        return jsonify(todo.to_dict()), 400
+    if 'deadline_at' in request.json:
+        todo.deadline_at = datetime.fromisoformat(request.json.get('deadline_at'))
+
+    # Adds a new record to the database or will update an existing record
+    db.session.add(todo)
+    # Commits the changes to the database, this must be called for the changes to be saved
+    db.session.commit()
+    return jsonify(todo.to_dict()), 201
+
 
 @api.route('/todos/<int:todo_id>', methods=['PUT'])
 def update_todo(todo_id):
-    """Update a todo item and return the updated item"""
-    return jsonify(TEST_ITEM)
+    todo = Todo.query.get(todo_id)
+    if todo is None:
+        return jsonify({'error': 'Todo not found'}), 404
+    extra = request.json.get('extra')
+    if todo.title is None or extra:
+        return jsonify(todo.to_dict()), 400
+    new_id = request.json.get('id')
+    if new_id is not None and new_id != todo_id:
+        return jsonify(todo.to_dict()), 400
+
+    todo.title = request.json.get('title', todo.title)
+    todo.description = request.json.get('description', todo.description)
+    todo.completed = request.json.get('completed', todo.completed)
+    todo.deadline_at = request.json.get('deadline_at', todo.deadline_at)
+    db.session.commit()
+    return jsonify(todo.to_dict())
+
 
 @api.route('/todos/<int:todo_id>', methods=['DELETE'])
 def delete_todo(todo_id):
-    """Delete a todo item and return the deleted item"""
-    return jsonify(TEST_ITEM)
- 
+    todo = Todo.query.get(todo_id)
+    if todo is None:
+        return jsonify({}), 200
+    db.session.delete(todo)
+    db.session.commit()
+    return jsonify(todo.to_dict()), 200
